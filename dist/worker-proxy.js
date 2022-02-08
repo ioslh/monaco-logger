55f23a743be94b59392d3917e031ccb5f158d3ee
ioslh <ohislh@gmail.com>
2022-02-08 23:24:14 +0800

    feat: add dist folder

---
+++ dist/worker-proxy.js
@@ -0,0 +1,4 @@
+self.MonacoEnvironment = {
+  baseUrl: '//cdn.jsdelivr.net/npm/monaco-editor@0.27.0/min/'
+};
+importScripts('//cdn.jsdelivr.net/npm/monaco-editor@0.27.0/min/vs/base/worker/workerMain.js');
